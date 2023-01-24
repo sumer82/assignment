b6e992fce830f6bd8d80c2cf4456febb8ac565dd
Sumer kumar <91982125+sumer82@users.noreply.github.com>
2023-01-24 15:51:19 +0530

    Add files via upload

---
+++ Assignment/src/Bank.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class Bank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Customer cust =new Customer("sumer","sumer123","sumer23","sin23","kolkata","sumer@mail.com",8210597901L,23);
+		Scanner scn =new Scanner(System.in);
+		
+		
+		System.out.println("******************** Welcome to My Bank *******************");
+		int choice;
+		String Username;
+		String Password;
+		int i=1;
+		
+		while(i==1) {
+			System.out.println("enter username :");
+			Username = scn.nextLine();
+			System.out.println("enter pssword :");
+			Password = scn.nextLine();		
+			if(cust.login(Username,Password)) {
+				System.out.println("login Sucessfull");
+				
+				int ch = 1;
+				while(ch == 1) {
+					
+					System.out.println("Please select the option");
+					System.out.println("1. Deposit");
+					System.out.println("2. Withdrawal");
+					System.out.println("3. Check balance");
+					System.out.println("4. Edit profile");
+					System.out.println("5. Change Password");
+					System.out.println("6. Exit");
+					
+					choice = scn.nextInt();
+					switch(choice) {
+					     
+					case 1: 
+						   System.out.println("enter the amout to be deposited");
+						   float amount =scn.nextFloat();
+						   System.out.println("updated amount is " + cust.deposit(amount));
+						   break;
+					case 2: 
+						   System.out.println("enter the amout to Withdraw");
+						   float amountW =scn.nextFloat();
+						   System.out.println("updated amount is " + cust.withdrawal(amountW));
+						   break;
+					case 3: 
+						   
+						   System.out.println("current Balance is " + cust.checkBalance());
+						   break;
+						   
+					case 4: 
+						   
+						   cust.editProfile();
+						   break;
+						   
+					case 5: 
+						   
+						   cust.changePassword();
+						   break;
+						   
+					case 6: 
+						   
+						   i--;
+						   ch--;
+						   break;
+						   
+				    default:
+				    	   System.out.println("sorry you pressed wrong button ");
+
+					
+					}
+					
+					
+				}
+				
+					
+				}
+			
+			
+			else {
+				
+				System.out.println("login failed");
+			}
+		
+		}
+
+	}
+
+}
---
+++ Assignment/src/Customer.java
@@ -0,0 +1,136 @@
+import java.util.Scanner;
+
+public class Customer {
+	
+	public String name;
+	public String Username;
+	public String Password;
+	public String SSn;
+	public String Address;
+	public String Email;
+	public long phone;
+	public float Balance;
+	public int age;
+	
+	public Customer(String name,String Username,String Password,String SSn,String Address,String email,long phone,int age) {
+		this.name = name;
+		this.Username = Username;
+		this.Password = Password;
+		this.SSn = SSn;
+		this.Address = Address;
+		this.Email = email;
+		this.phone = phone;
+		this.Balance =100;
+		this.age=age;
+		
+		
+		
+	}
+	
+	public Boolean login(String Username,String Password) {
+		if(this.Username.equals(Username)) {
+			if(this.Password.equals(Password)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public float deposit(float amount) {
+		this.Balance = Balance+amount;
+		return this.Balance;
+	}
+	
+	public float withdrawal(float amount) {
+		this.Balance = Balance-amount;
+		return this.Balance;
+	}
+	
+	public float checkBalance() {
+		return this.Balance;
+	}
+	
+	public void editProfile() {
+		System.out.println(" Please select the option");
+		System.out.println("1. change name");
+		System.out.println("2. change username");
+		System.out.println("3. change address");
+		System.out.println("4. change email");
+		System.out.println("5. change phone");
+		System.out.println("6. change age");
+		
+		int choice;
+		Scanner scn = new Scanner(System.in);
+		choice = scn.nextInt();
+		switch(choice) {
+		
+		case 1:
+			System.out.println("enter new name");
+			String newName = scn.nextLine();
+			this.name = newName;
+			System.out.println("name updated ");
+			break;
+			
+		case 2:
+			System.out.println("enter new username");
+			String newUsername = scn.nextLine();
+			this.Username = newUsername;
+			System.out.println("Username updated ");
+			break;
+			
+		case 3:
+			System.out.println("enter new address");
+			String newAddress = scn.nextLine();
+			this.Address = newAddress;
+			System.out.println("address updated ");
+			break;
+			
+		case 4:
+			System.out.println("enter new email");
+			String newEmail = scn.nextLine();
+			this.Email = newEmail;
+			System.out.println("Email updated ");
+			break;
+			
+		case 5:
+			System.out.println("enter new phone no");
+			long newPhone = scn.nextLong();
+			this.phone = newPhone;
+			System.out.println("Phone no updated");
+			break;
+		case 6:
+			System.out.println("enter new age");
+			int newAge = scn.nextInt();
+			this.age = newAge;
+			System.out.println("Age updated");
+			break;
+			
+		default:
+			System.out.println("you entered something wrong");
+		       
+		
+		}
+	
+	}
+	public void changePassword() {
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("Enter current Password");
+		String pass = scn.nextLine();
+		if(this.Password.equals(pass)) {
+			System.out.println("Enter new Password");
+			String passNew1 = scn.nextLine();
+			System.out.println("confirm new Password");
+			String passNew2 = scn.nextLine();
+			if(passNew1.equals(passNew2)) {
+				this.Password = passNew1;
+				System.out.println("Password changed sucessfully");
+			}
+			else {
+				System.out.println("Passwords did not match");
+			}
+		}
+		System.out.println("password incorrect");
+	}
+
+}
