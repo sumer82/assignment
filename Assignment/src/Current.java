af2ecc797c956b14e8ac143121fdb3add42148a0
Sumer kumar <91982125+sumer82@users.noreply.github.com>
2023-01-27 14:14:17 +0530

    Update Current.java
    added abstract methods implementation

--- Assignment/src/Current.java
+++ Assignment/src/Current.java
@@ -1,11 +1,11 @@
+
 public class Current extends Account{
 
-     public void display(){
-           super.minBalance = 0;
-           System.out.println("min balance needed for current account is "+super.minBalance);
-     }
+	@Override
+	void display() {
+		  super.minBalance = 0;
+          System.out.println("min balance needed for current account is "+super.minBalance);
+		
+	}
 
-     
-
-     
 }
