6b029e57bcfe3eb01a69b89ff9a02b4edf2cf487
Sumer kumar <91982125+sumer82@users.noreply.github.com>
2023-01-27 14:13:26 +0530

    Update Account.java
    added abstract class

--- Assignment/src/Account.java
+++ Assignment/src/Account.java
@@ -1,13 +1,10 @@
-
-public class Account{
+abstract class Account{
 
     public int accountId = 123;
     public String Description ="bank account";
     public float minBalance;
 
-   public void display(){
-     System.out.println("account details are" + this.accountId +this.Description);
-    }
+   abstract void display();
 
 
 }
